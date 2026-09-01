db1451efae5cfa413ed1b5cb9964068b0da9f1f9
Astuto7 <joseroberto1953o@gmail.com>
2026-08-31 22:11:29 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'chave-secreta-oficina-777')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'chave-oficina-segura-2026')
     
-    db_url = os.environ.get('DATABASE_URL', 'sqlite:///oficina.db')
-    if db_url.startswith("postgres://"):
-        db_url = db_url.replace("postgres://", "postgresql://", 1)
+    # Tratamento automático de URI para SQLite local e PostgreSQL no Render
+    raw_db_url = os.environ.get('DATABASE_URL', 'sqlite:///oficina.db')
+    if raw_db_url.startswith("postgres://"):
+        raw_db_url = raw_db_url.replace("postgres://", "postgresql://", 1)
         
-    SQLALCHEMY_DATABASE_URI = db_url
+    SQLALCHEMY_DATABASE_URI = raw_db_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER_QR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static', 'qr')
