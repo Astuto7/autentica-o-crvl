7e4b624825ee014024869a6038b30c7dabc9936e
Astuto7 <joseroberto1953o@gmail.com>
2026-08-31 22:13:33 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,1 +1,11 @@
+from flask import Blueprint, render_template, abort
+from models import AuthRecord
 
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/v/<token>')
+def view_record(token):
+    registro = AuthRecord.query.filter_by(token=token).first()
+    if not registro:
+        abort(404)
+    return render_template('auth/view.html', registro=registro)
